f349a55ab8f93378690071d3460e77c8d6f6c768
Budkov Denis <Chukcha2017@bk.ru>
2025-01-12 23:15:13 +0400

    Разбили js на модули.

---
+++ qoutes.js
@@ -0,0 +1,56 @@
+const quotes = [
+    {
+      quote: "The only way to do great work is to love what you do",
+      author: "Steve Jobs",
+    },
+    {
+      quote: "Innovation distinguishes between a leader and a follower",
+      author: "Steve Jobs",
+    },
+    {
+      quote: "Your time is limited, so don't waste it living someone else's life",
+      author: "Steve Jobs",
+    },
+    {
+      quote:
+        "Success is not final, failure is not fatal: It is the courage to continue that counts",
+      author: "Winston Churchill",
+    },
+    {
+      quote: "Life is what happens when you're busy making other plans",
+      author: "John Lennon",
+    },
+    {
+      quote:
+        "Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment",
+      author: "Buddha",
+    },
+    {
+      quote: "It always seems impossible until it’s done",
+      author: "Nelson Mandela",
+    },
+    {
+      quote:
+        "Happiness is not something ready-made. It comes from your own actions",
+      author: "Dalai Lama",
+    },
+    {
+      quote: "In the middle of every difficulty lies opportunity",
+      author: "Albert Einstein",
+    },
+    {
+      quote:
+        "What lies behind us and what lies before us are tiny matters compared to what lies within us",
+      author: "Ralph Waldo Emerson",
+    },
+    {
+      quote: "Believe you can and you're halfway there",
+      author: "Theodore Roosevelt",
+    },
+    {
+      quote: "Don’t watch the clock; do what it does. Keep going",
+      author: "Sam Levenson",
+    },
+  ];
+
+  export default quotes;
