5234c8b1a1c9ee466155abfeb590c6194daf6c41
Budkov Denis <Chukcha2017@bk.ru>
2025-01-16 21:58:03 +0400

    создание кнопки для цитат, которые понравились.

--- qoutes.js
+++ qoutes.js
@@ -2,54 +2,66 @@ const quotes = [
     {
       quote: "The only way to do great work is to love what you do",
       author: "Steve Jobs",
+      isFavorite: false,
     },
     {
       quote: "Innovation distinguishes between a leader and a follower",
       author: "Steve Jobs",
+      isFavorite: false,
     },
     {
       quote: "Your time is limited, so don't waste it living someone else's life",
       author: "Steve Jobs",
+      isFavorite: false,
     },
     {
       quote:
         "Success is not final, failure is not fatal: It is the courage to continue that counts",
       author: "Winston Churchill",
+      isFavorite: false,
     },
     {
       quote: "Life is what happens when you're busy making other plans",
       author: "John Lennon",
+      isFavorite: false,
     },
     {
       quote:
         "Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment",
       author: "Buddha",
+      isFavorite: false,
     },
     {
       quote: "It always seems impossible until it’s done",
       author: "Nelson Mandela",
+      isFavorite: false,
     },
     {
       quote:
         "Happiness is not something ready-made. It comes from your own actions",
       author: "Dalai Lama",
+      isFavorite: false,
     },
     {
       quote: "In the middle of every difficulty lies opportunity",
       author: "Albert Einstein",
+      isFavorite: false,
     },
     {
       quote:
         "What lies behind us and what lies before us are tiny matters compared to what lies within us",
       author: "Ralph Waldo Emerson",
+      isFavorite: false,
     },
     {
       quote: "Believe you can and you're halfway there",
       author: "Theodore Roosevelt",
+      isFavorite: false,
     },
     {
       quote: "Don’t watch the clock; do what it does. Keep going",
       author: "Sam Levenson",
+      isFavorite: false,
     },
   ];
 
--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-import quotes from "./qoutes.js";  
+import quotes from "./qoutes.js";
 
 const quoteElement = document.getElementById("quote");
 const quoteAuthorElement = document.getElementById("author");
 const generateBtn = document.getElementById("generate-btn");
+const toggleFavoriteBtn = document.getElementById("toggle-favorite-btn");
+
+
+let currentQuoteIndex;
 
 function generateRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
+  currentQuoteIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[currentQuoteIndex];
   const { quote, author } = randomQuote;
   // const quote = randomQuote.quote;
   // const quoteAuthor = randomQuote.author;
@@ -16,7 +20,23 @@ function generateRandomQuote() {
   quoteAuthorElement.textContent = author;
 }
 
+function toggleFavorite() {
+  const currentQuote = quotes[currentQuoteIndex];                      //Поиск обьекта в массиве обьектов.
+  currentQuote.isFavorite = !currentQuote.isFavorite;
+
+  // if (toggleFavoriteBtn.textContent === "Add to favorites") {
+  //   toggleFavoriteBtn.textContent = "Remove from favorites";
+  // } else {
+  //   toggleFavoriteBtn.textContent = "Add to favorites";
+  // }
+
+  toggleFavoriteBtn.textContent = currentQuote.isFavorite                 //Изменение названия кнопки.
+    ? "Remove from favorites"
+    : "Add to favorites";
+}
+
 generateBtn.addEventListener("click", generateRandomQuote);
+toggleFavoriteBtn.addEventListener("click", toggleFavorite);
 
 // Цитаты будут высвечиваться при загрузке страницы
 
