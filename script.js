105ef0ce71db77e6676d80df910650490fa58abc
Budkov Denis <Chukcha2017@bk.ru>
2025-01-17 22:42:34 +0400

    Создание карточек для цитат, которые понравились.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const quoteElement = document.getElementById("quote");
 const quoteAuthorElement = document.getElementById("author");
 const generateBtn = document.getElementById("generate-btn");
 const toggleFavoriteBtn = document.getElementById("toggle-favorite-btn");
-
+const favoritesContainer = document.getElementById("favorites-container");
 
 let currentQuoteIndex;
 
@@ -18,10 +18,15 @@ function generateRandomQuote() {
   //const quote = `"<em>${randomQuote.quote}"</em><br>${randomQuote.author}`;
   quoteElement.textContent = quote;
   quoteAuthorElement.textContent = author;
+  toggleFavoriteBtn.textContent = randomQuote.isFavorite //Изменение названия кнопки.
+    ? "Remove from favorites"
+    : "Add to favorites";
+
+  toggleFavoriteBtn.style.display = "inline-block"; //Кнопка появляется, когда появляются цитаты.
 }
 
 function toggleFavorite() {
-  const currentQuote = quotes[currentQuoteIndex];                      //Поиск обьекта в массиве обьектов.
+  const currentQuote = quotes[currentQuoteIndex]; //Поиск обьекта в массиве обьектов.
   currentQuote.isFavorite = !currentQuote.isFavorite;
 
   // if (toggleFavoriteBtn.textContent === "Add to favorites") {
@@ -30,9 +35,23 @@ function toggleFavorite() {
   //   toggleFavoriteBtn.textContent = "Add to favorites";
   // }
 
-  toggleFavoriteBtn.textContent = currentQuote.isFavorite                 //Изменение названия кнопки.
+  toggleFavoriteBtn.textContent = currentQuote.isFavorite //Изменение названия кнопки.
     ? "Remove from favorites"
     : "Add to favorites";
+
+  if (currentQuote.isFavorite) {
+    const favoriteCard = document.createElement("div");
+    favoriteCard.classList.add("favorite-card");
+    favoriteCard.innerHTML = `<p>${currentQuote.quote}</p> <p class="author">${currentQuote.author}</p>`;
+    favoritesContainer.appendChild(favoriteCard);
+  } else {
+    const favoriteCards = document.querySelectorAll(".favorite-card");
+    favoriteCards.forEach((card) => {
+      if (card.textContent.includes(currentQuote.quote)) {
+        card.remove();
+      }
+    });
+  }
 }
 
 generateBtn.addEventListener("click", generateRandomQuote);
